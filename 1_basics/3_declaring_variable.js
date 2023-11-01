ef37be4530a2332f35c4b8d8b97425467b7611ad
2maeng <vbn3701@gmail.com>
2023-11-01 14:39:21 +0900

    JS 변수 선언 및 데이터 타입

---
+++ 1_basics/3_declaring_variable.js
@@ -0,0 +1,72 @@
+/**
+ * Variable 선언하기
+ *
+ * 1) var - 더이상 쓰지 않는다.
+ * 2) let
+ * 4) const
+ */
+
+var name = '코드팩토리';
+console.log(name);
+
+var age = 32;
+console.log(age);
+
+let ive = '아이브';
+console.group(ive);
+
+/**
+ * let과 var로 선언하면
+ * 값을 추후 변경할 수 있다.
+ */
+
+ive = '안유진';
+console.log(ive);
+
+const newJeans = '뉴진스'; // const는 값이 변경되지 않는다.
+console.log(newJeans);
+
+// newJeans = '코드팩토리';
+
+/**
+ * 선언과 할당
+ * 
+ * 1) 변수를 선언하는 것.
+ * 2) 할당 : 값을 넣어주는 것
+ */
+var name = '코드팩토리';
+console.log(name);
+
+let girlFriend;
+console.log(girlFriend);
+
+const girlFriend2 = '';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
